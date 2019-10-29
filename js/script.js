2f0f3c8e75ba824600fca6fd1da0e86e68f4eb87
svetaelar <svetaelar@gmail.com>
2019-10-29 18:31:11 +0300

    добавила табиндекс

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 var btnFeedback = document.querySelector (".button-feedback");
 var popupFeedback = document.querySelector (".modal-feedback");
-var btnCloseFeedback = popupFeedback.querySelector (".button-close-feedback");
-var loginFeedback = popupFeedback.querySelector ("#feedback-name");
-var emailFeedback = popupFeedback.querySelector ("#feedback-email");
-var textFeedback = popupFeedback.querySelector ("#feedback-text");
-var formFeedback = popupFeedback.querySelector ("form");
-var sliderControls = document.querySelectorAll (".controls");
-var sliderList = document.querySelectorAll (".slider-item");
+var btnCloseFeedback = popupFeedback.querySelector(".button-close-feedback");
+var loginFeedback = popupFeedback.querySelector("#feedback-name");
+var emailFeedback = popupFeedback.querySelector("#feedback-email");
+var textFeedback = popupFeedback.querySelector("#feedback-text");
+var formFeedback = popupFeedback.querySelector("form");
+var sliderControls = document.querySelectorAll(".controls");
+var sliderList = document.querySelectorAll(".slider-item");
 var background = ["#849d8f", "#8c9aaa", "#a18f88"];
-var body = document.querySelector ("body")
+var body = document.querySelector("body")
 
-sliderControls[0].addEventListener ("click", function(){
+sliderControls[0].addEventListener("click", function(){
   sliderClick(0);
 });
-sliderControls[1].addEventListener ("click", function(){
+sliderControls[1].addEventListener("click", function(){
   sliderClick(1);
 });
-sliderControls[2].addEventListener ("click", function(){
+sliderControls[2].addEventListener("click", function(){
   sliderClick(2);
 });
 
 function sliderClick(control){
     body.style.background = background[control];
     for (var i=0; i<sliderControls.length; i++){
-      sliderList[i].classList.remove ("slide-show");
-      sliderControls[i].classList.remove ("controls-active");
+      sliderList[i].classList.remove("slide-show");
+      sliderControls[i].classList.remove("controls-active");
     }
-    sliderList[control].classList.add ("slide-show");
-    sliderControls[control].classList.add ("controls-active");
+    sliderList[control].classList.add("slide-show");
+    sliderControls[control].classList.add("controls-active");
 }
 
-btnFeedback.addEventListener ("click", function(evt){
+btnFeedback.addEventListener("click", function(evt){
     evt.preventDefault();
     popupFeedback.classList.add("modal-show");
 })
 
-btnCloseFeedback.addEventListener ("click", function(evt){
+btnCloseFeedback.addEventListener("click", function(evt){
     evt.preventDefault();
     popupFeedback.classList.remove("modal-show");
     popupFeedback.classList.remove("modal-error");
 })
 
-formFeedback.addEventListener("submit", function (evt) {
-    if (!loginFeedback.value || !emailFeedback.value || !textFeedback.value) {
+formFeedback.addEventListener("submit", function (evt){
+    if (!loginFeedback.value || !emailFeedback.value || !textFeedback.value){
       evt.preventDefault();
       popupFeedback.classList.remove("modal-error");
       popupFeedback.offsetWidth = popupFeedback.offsetWidth;
@@ -51,10 +51,10 @@ formFeedback.addEventListener("submit", function (evt) {
     }
 });
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
+window.addEventListener("keydown", function (evt){
+    if (evt.keyCode === 27){
       evt.preventDefault();
-      if (popupFeedback.classList.contains("modal-show")) {
+      if (popupFeedback.classList.contains("modal-show")){
         popupFeedback.classList.remove("modal-show");
         popupFeedback.classList.remove("modal-error");
       }
